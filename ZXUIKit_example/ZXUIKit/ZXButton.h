471cb35edc1888f5c881591b62790f223179720c
李主贤 <lizhuxian020@pingan.com.cn>
2017-04-12 21:01:55 +0800

    create ZXButton & input in proj

---
+++ ZXUIKit_example/ZXUIKit/ZXButton.h
@@ -0,0 +1,13 @@
+//
+//  ZXButton.h
+//  ZXUIKit_example
+//
+//  Created by lzx on 17/4/12.
+//  Copyright © 2017年 lzx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZXButton : UIView
+
+@end
---
+++ ZXUIKit_example/ZXUIKit/ZXButton.m
@@ -0,0 +1,21 @@
+//
+//  ZXButton.m
+//  ZXUIKit_example
+//
+//  Created by lzx on 17/4/12.
+//  Copyright © 2017年 lzx. All rights reserved.
+//
+
+#import "ZXButton.h"
+
+@implementation ZXButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
